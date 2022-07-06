aba2ed5713be44c25e4cd3a80c9aff965b4a8f21
LucaHofstetter <luca.marco.hofstetter@gmail.com>
2022-07-06 10:40:09 +0200

    went through first tutorial to learn basics

--- dev-portfolio/src/main.js
+++ dev-portfolio/src/main.js
@@ -1,32 +1,24 @@
-import { Camera, Material, Texture } from "https://cdn.skypack.dev@0.132.2";
-import { OrbitControls } from "https://cdn.skypack.dev@0.132.2/examples/jsm/controls/OrbitControls.js";
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-var testScene = new THREE.Scene();
-var testCamera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-var testRenderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-testRenderer.setSize(window.innerWidth, window.innerHeight);
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-document.body.appendChild(testRenderer.domElement);
-
-// CREATE CUBE (for first practice)
-var geometry = new THREE.BoxGeometry(1,1,1);
-var material = new THREE.MeshBasicMaterial( {
-  color: #fff
-});
-var cube = new THREE.Mesh(geometry, material);
-
-testScene.add(cube);
 camera.position.z = 5;
 
-var animate = () => {
-  requestAnimationFrame(animate);
+var animate = function () {
+	requestAnimationFrame( animate );
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
 
-  testRenderer.render(scene, camera);
-
+	renderer.render( scene, camera );
 };
 
 animate();
